ba0d188fd0694e065f9cb5173495f30da1883ee2
vux5561 <58337958+vux5561@users.noreply.github.com>
2019-11-30 00:11:12 +0700

    Update and rename py2.py to py3.py

--- py3.py
+++ py3.py
@@ -392,7 +392,7 @@ def PasswordSection(password="0000", path="0", tracking_string="Home"):
 		return plugin.finish(items)
 	else:
 		xbmc.executebuiltin('Notification("%s", "%s", "%d", "%s")' %
-			                    (time.time(), passwords[password], 10000, ''))
+			                    (password, passwords[password], 10000, ''))
 		passw_string = plugin.keyboard(heading='Nhập password')
 		if passw_string <> password:
 			passwords[password] = time.time()
